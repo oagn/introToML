5979f5a1fd5c5c0c70ac8bc6577dea858c4465c7
oagn <agnethe.s.olsen@gmail.com>
2018-07-29 20:46:14 +0100

    Initial commit for gaussianNB test file in PyCharm project

---
+++ naiveBayes/gaussianNB.py
@@ -0,0 +1,9 @@
+# First test program
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+clf = GaussianNB()
+clf.fit(X, Y)
+print(clf.predict([[-0.8, -1]]))
