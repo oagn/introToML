cec548acfed13e2c4433d485e7fe0f5e66bf9977
oagn <agnethe.s.olsen@gmail.com>
2018-07-29 21:23:28 +0100

    First task, Gaussian deployment, example w python2 which causes issues when creating image

--- naiveBayes/gaussianNB.py
+++ naiveBayes/gaussianNB.py
@@ -1,9 +1,14 @@
-# First test program
+# First test program, supervised classification
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 
+# Features
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+# Labels
 Y = np.array([1, 1, 1, 2, 2, 2])
+
+# Classifier
 clf = GaussianNB()
+
 clf.fit(X, Y)
 print(clf.predict([[-0.8, -1]]))
